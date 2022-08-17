5ab92bbd6223915c8861c6141a30ed4f8af9945b
tdbooneABK <trevor.boone@activision.com>
2022-08-17 10:13:25 -0700

    Implement Door Blueprint

---
+++ Source/MazeGameV3/Private/DoorInteractionComponent.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DoorInteractionComponent.h"
+#include "Engine/TriggerBase.h"
+
+// Sets default values for this component's properties
+UDoorInteractionComponent::UDoorInteractionComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UDoorInteractionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	StartRotation = GetOwner()->GetActorRotation();
+	FinalRotation = GetOwner()->GetActorRotation() + DesiredRotation;
+
+	CurrentRotationTime = 0.0f;
+}
+
+
+// Called every frame
+void UDoorInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (CurrentRotationTime < TimeToRotate) {
+		if (DoorOpenTrigger && GetWorld() && GetWorld()->GetFirstLocalPlayerFromController())
+		{
+			APawn* PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+			if (PlayerPawn && DoorOpenTrigger->IsOverlappingActor(PlayerPawn)) {
+				CurrentRotationTime += DeltaTime;
+				const float TimeRatio = FMath::Clamp(CurrentRotationTime / TimeToRotate, 0.0f, 1.0f);
+				const float RotationAlpha = DoorOperCurve.GetRichCurveConst()->Eval(TimeRatio);
+				const FRotator CurrentRotation = FMath::Lerp(StartRotation, FinalRotation, RotationAlpha);
+				GetOwner()->SetActorRotation(CurrentRotation);
+			}
+		}
+	}	
+}
+
